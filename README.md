7536983338149401704cab538acc98f2dc824742
Suraj Jayakumar <suraj.jayakumar@gmail.com>
2016-08-02 11:15:48 +0530

    Added Main Code
    Use Main.py  or temp.py to calculate the weights

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # language-modeling
+
+
+## Getting and Cleaning Data
+
+Data has 600901 characters, 59 unique.
+
+
+Each letter is represented using a **One Hot Vector** of the size of the total vocabulary.
+
+Example:
+Suppose there are 59 unique chars in the vocabulary
+
+'c' => `a =   [0,0,0,0,1,0,0,0,0,0,,....]`
+where a[4] = 1 represents the character 'c'
+
+Number of training samples: 500000
+Number of testing samples: 100000
+
+X : (60000L, 28L, 28L)
+y : (60000L, 10L)
+
+
+
+## Proposed Neural Network Architecture
+
+____________________________________________________________________________________________________
+Layer (type)                     Output Shape          Param #     Connected to
+====================================================================================================
+InputLayer			             (None, 1, 59)         0
+____________________________________________________________________________________________________
+LSTM 1		                     (None, 1, 128)        96256       InputLayer
+____________________________________________________________________________________________________
+LSTM 2		                     (None, 64)            49408       LSTM 1
+____________________________________________________________________________________________________
+Dense 		                     (None, 59)            3835        LSTM 2
+====================================================================================================
+Total params: 149499
+____________________________________________________________________________________________________
+
+
+## Statistics and Sample Output
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ Created on Mon Jul 25 08:40:19 2016
 
 import numpy as np
 
-from keras.models import Model,model_from_json
+from keras.models import Model,model_from_json, Sequential
 from keras.layers import Input, Dense, Dropout, LSTM
+from keras.utils.data_utils import get_file
 
 
 ##################################
@@ -17,37 +18,36 @@ X_train =0
 X_test = 0
 Y_train = 0
 Y_test = 0
-model = 0 
-
-
-
+model = 0
 
 
 # data I/O
-data = open('input.txt', 'r').read() # should be simple plain text file
+path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
+data= open(path).read().lower()
+
+#data = open('input.txt', 'r').read() # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
 char_to_ix = { ch:i for i,ch in enumerate(chars) }
 ix_to_char = { i:ch for i,ch in enumerate(chars) }
 
-data_size = 50000
 
 train_samples = int(data_size*0.8)
 
 def create_data():
     global X_train,X_test,Y_train,Y_test
 
-        
+
     X = np.zeros((data_size,1,vocab_size))
     y = np.zeros((data_size,vocab_size))
-    
-    
+
+
     for i in xrange(data_size):
         this_char = data[i]
         next_char = data[(i+1)%data_size]
-        X[i][0][char_to_ix[this_char]] = 1
-        y[i][char_to_ix[next_char]] = 1
+        X[i][0][char_to_ix[this_char]] = 1.
+        y[i][char_to_ix[next_char]] = 1.
 
     X_train = X
     Y_train = y
@@ -56,31 +56,38 @@ def create_data():
 
 
 def build_model():
-    
+
     global model
-    
+
     input_layer = Input(shape=(1,vocab_size))
-    
+
     lstm_1 = LSTM(128,return_sequences=True)(input_layer)
-    
+
     lstm_2 = LSTM(64)(lstm_1)
 
-    output_layer = Dense(vocab_size,activation='softmax')(lstm_2)    
+    output_layer = Dense(vocab_size,activation='softmax')(lstm_2)
 
     model = Model(input=input_layer,output=output_layer)
+
+#    print('Build model...')
+#    model = Sequential()
+#    model.add(LSTM(128, input_shape=(1, vocab_size)))
+#    model.add(Dense(vocab_size,activation='softmax'))
+
+
     model.summary()
     model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
-              metrics=['accuracy']) 
-              
-    
+              metrics=['accuracy'])
+
+
 def train_model():
     #verbose = 1 for progress bar logging, 2 for one log line per epoch.
     global model
     model.fit(X_train, Y_train,
                     batch_size=1, nb_epoch=10,
                     verbose=2)
-                    
+
     score = model.evaluate(X_test, Y_test, verbose=0)
     print score
 
@@ -97,14 +104,14 @@ def load_model():
     model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-              
-              
+
+
 def print_model_output(start,end):
-    
+
     final_str=""
 
 if __name__=="__main__":
     create_data()
     build_model()
     train_model()
-    save_model()    
+    save_model()
